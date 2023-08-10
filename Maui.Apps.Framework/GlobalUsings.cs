05910b6053b5cdc62412109c3208289b1a30a5f6
STGorbunovDA <bous07@mail.ru>
2023-08-10 21:30:36 +0300

    Добавьте файлы проекта.

---
+++ Maui.Apps.Framework/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System.Text.RegularExpressions;
+global using MonkeyCache;
+global using Maui.Apps.Framework.Exceptions;
+global using Maui.Apps.Framework.Extensions;
+global using System.Text;
+global using System.Text.Json;
---
+++ YoutubePlayer/Views/StartPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace YoutubePlayer.Views;
+
+public partial class StartPage : ContentPage
+{
+	public StartPage()
+	{
+		InitializeComponent();
+	}
+}
