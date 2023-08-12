69ab3aea0d38f4e92b6ce3bccbf50232585c5eea
STGorbunovDA <bous07@mail.ru>
2023-08-12 20:52:53 +0300

    Добавление ContentDisplayMode, нужен для отображения контента за навигационной панелью

---
+++ Maui.Apps.Framework/UI/ContentDisplayMode.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui.Apps.Framework.UI
+{
+    /// <summary>
+    /// Отображение контента, связанного с навигационной панелью (контент за ней или нет).
+    /// </summary>
+    public enum ContentDisplayMode
+    {
+        NoNavigationBar,
+        NavigationBar
+    }
+}
