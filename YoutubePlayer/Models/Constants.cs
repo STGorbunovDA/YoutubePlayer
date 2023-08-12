09bb5e33d198654fa0ce87583e392401403aed4f
STGorbunovDA <bous07@mail.ru>
2023-08-12 21:53:11 +0300

    Создение Constants приложения

---
+++ YoutubePlayer/Models/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace YoutubePlayer.Models
+{
+    public static class Constants
+    {
+        public static string ApplicationName = "YouTubePlayer";
+        public static string EmailAddress = @"bous07@mail.ru";
+        public static string ApplicationId = ""; //"XGENO.TubePlayer.App";
+        public static string ApiServiceURL = @"https://youtube.googleapis.com/youtube/v3/";
+        public static string ApiKey = @"AIzaSyAuULBprJ_RJljoK1v5keXBEomAseowTio"; //@"AIzaSyAT7UDu2LYMDWHPP09sD7Rq6G-lUSX4CA8";
+
+        //Время анимации
+        public static uint MicroDuration { get; set; } = 100;
+        public static uint SmallDuration { get; set; } = 300;
+        public static uint MediumDuration { get; set; } = 600;
+        public static uint LongDuration { get; set; } = 1200;
+        public static uint ExtraLongDuration { get; set; } = 1800;
+    }
+}
