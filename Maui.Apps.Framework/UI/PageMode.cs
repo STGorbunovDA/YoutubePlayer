f412a007727f300db430f79cd725f4c840024140
STGorbunovDA <bous07@mail.ru>
2023-08-12 20:58:44 +0300

    Добавить PageMode для разного взаимоействия с  menu

---
+++ Maui.Apps.Framework/UI/PageMode.cs
@@ -0,0 +1,13 @@
+﻿namespace Maui.Apps.Framework.UI
+{
+    /// <summary>
+    /// Для отображения меню
+    /// </summary>
+    public enum PageMode
+    {
+        None,
+        Menu,
+        Navigate,
+        Modal
+    }
+}
