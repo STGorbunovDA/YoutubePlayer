6058ff3fa74ed42e73a496de29ce58d24c7587f3
STGorbunovDA <bous07@mail.ru>
2023-08-12 08:29:58 +0300

    Реализация ServiceHelper для получения провайдера служб.

---
+++ YoutubePlayer/Helpers/ServiceHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace YoutubePlayer.Helpers
+{
+    /// <summary>
+    /// Получение провайдера служб
+    /// </summary>
+    public static class ServiceHelper
+    {
+        /// <summary>
+        /// Возвращает текущего провайдера служб
+        /// </summary>
+        public static TService GetService<TService>() => //test
+        Current.GetService<TService>();
+
+        public static IServiceProvider Current =>
+#if WINDOWS10_0_17763_0_OR_GREATER
+			MauiWinUIApplication.Current.Services;
+#elif ANDROID
+    MauiApplication.Current.Services;
+#elif IOS || MACCATALYST
+                MauiUIApplicationDelegate.Current.Services;
+#else
+			null;
+#endif
+    }
+}
