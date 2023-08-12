eaf51c2ec01d1336b1dd69f4605f82ddae2afc38
STGorbunovDA <bous07@mail.ru>
2023-08-12 08:18:29 +0300

    Частичная реализация ViewModelBase

---
+++ Maui.Apps.Framework/MVVM/ViewModelBase.cs
@@ -0,0 +1,58 @@
+﻿namespace Maui.Apps.Framework.MVVM
+{
+    /// <summary>
+    /// Изменение модели для MVVM
+    /// </summary>
+    public partial class ViewModelBase : ObservableObject
+    {
+        [ObservableProperty]
+        private string title = string.Empty;
+
+        [ObservableProperty]
+        private bool isBusy = false;
+
+        [ObservableProperty]
+        private string loadingText = string.Empty;
+
+        [ObservableProperty]
+        private bool dataLoaded = false;
+
+        [ObservableProperty]
+        private bool isErrorState = false;
+
+        [ObservableProperty]
+        private string errorMessage = string.Empty;
+
+        [ObservableProperty]
+        private string errorImage = string.Empty;
+
+        public ViewModelBase() =>
+            IsErrorState = false;
+
+        /// <summary>
+        /// Вызывается при появлении страницы
+        /// </summary>
+        public virtual async void OnNavigatedTo(object parameters) =>
+            await Task.CompletedTask;
+
+        /// <summary>
+        /// Индикаторы загрузки для страницы
+        /// </summary>
+        protected void SetDataLodingIndicators(bool isStaring = true)
+        {
+            if (isStaring)
+            {
+                IsBusy = true;
+                DataLoaded = false;
+                IsErrorState = false;
+                ErrorMessage = "";
+                ErrorImage = "";
+            }
+            else
+            {
+                LoadingText = "";
+                IsBusy = false;
+            }
+        }
+    }
+}
