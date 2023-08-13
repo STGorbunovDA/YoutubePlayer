dd1b189ee2894d822b720997a68df8c9f144e142
STGorbunovDA <bous07@mail.ru>
2023-08-13 20:39:05 +0300

    Реализация интерфейса IApiService с протоколом поиска видео SearchVideos

---
+++ YoutubePlayer/IServices/IApiService.cs
@@ -0,0 +1,11 @@
+﻿namespace YoutubePlayer.IServices
+{
+    public interface IApiService
+    {
+        /// <summary>
+        /// Представляет результат поиска видео, хранящий всю необходимую информацию 
+        /// о найденных видео, такую как заголовки, длительность, авторы и т. д.
+        /// </summary>
+        Task<VideoSearchResult> SearchVideos(string searchQuery, string nextPageToken = "");
+    }
+}
