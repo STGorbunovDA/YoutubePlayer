cb7be51506dccc589cd0e2587f6608f0584fc7ff
STGorbunovDA <bous07@mail.ru>
2023-08-13 20:40:47 +0300

    Создание YoutubeService с установление базового ApiServiceURL и реализации протокола поиска видео SearchVideos

--- YoutubePlayer/GlobalUsings.cs
+++ YoutubePlayer/GlobalUsings.cs
@@ -1,1 +1,7 @@
 ﻿global using YoutubePlayer.Views;
+global using System.Text.Json.Serialization;
+global using YoutubePlayer.Models;
+global using Maui.Apps.Framework.Services;
+global using MonkeyCache;
+global using System.Net;
+global using YoutubePlayer.IServices;
---
+++ YoutubePlayer/Services/YoutubeService.cs
@@ -0,0 +1,29 @@
+﻿namespace YoutubePlayer.Services
+{
+    public class YoutubeService : RestServiceBase, IApiService
+    {
+        public YoutubeService(IConnectivity connectivity, IBarrel cacheBarrel) : base(connectivity, cacheBarrel)
+        {
+            // устанавливается базовый URL
+            SetBaseURL(Constants.ApiServiceURL);
+        }
+
+        /// <summary>
+        /// Содержит параметры запроса, включая API ключ, закодированную строку поиска и, 
+        /// если есть, токен следующей страницы.
+        /// Затем, с помощью метода GetAsync класса RestServiceBase, 
+        /// отправляется GET-запрос на YouTube API с использованием сформированного 
+        /// URL resourceUri и числа 4 (количество попыток повторной попытки при ошибке).
+        /// </summary>
+        public async Task<VideoSearchResult> SearchVideos(string searchQuery, string nextPageToken = "")
+        {
+            var resourceUri = $"search?part=snippet&maxResults=10&type=video&key={Constants.ApiKey}&q={WebUtility.UrlEncode(searchQuery)}"
+            +
+            (!string.IsNullOrEmpty(nextPageToken) ? $"&pageToken={nextPageToken}" : "");
+
+            var result = await GetAsync<VideoSearchResult>(resourceUri, 4);
+
+            return result;
+        }
+    }
+}
