5de0f48052ae4539b972899cdfbe22b30a024ed4
STGorbunovDA <bous07@mail.ru>
2023-08-29 21:31:11 +0900

    Добавить ApplicationId

--- YoutubePlayer/Models/Constants.cs
+++ YoutubePlayer/Models/Constants.cs
@@ -4,7 +4,7 @@
     {
         public static string ApplicationName = "YouTubePlayer";
         public static string EmailAddress = @"bous07@mail.ru";
-        public static string ApplicationId = ""; //"XGENO.TubePlayer.App";
+        public static string ApplicationId = "GDA.YouTubePlayer.App"; //"XGENO.TubePlayer.App";
         public static string ApiServiceURL = @"https://youtube.googleapis.com/youtube/v3/";
         public static string ApiKey = @"AIzaSyAuULBprJ_RJljoK1v5keXBEomAseowTio"; //@"AIzaSyAT7UDu2LYMDWHPP09sD7Rq6G-lUSX4CA8";
 
