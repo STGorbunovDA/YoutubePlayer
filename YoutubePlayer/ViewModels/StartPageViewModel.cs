ae81cd653fb19bf4ea5fca4f3f8e079f9f252ce8
STGorbunovDA <bous07@mail.ru>
2023-08-30 00:57:59 +0900

    StartPageViewModel

--- YoutubePlayer/ViewModels/StartPageViewModel.cs
+++ YoutubePlayer/ViewModels/StartPageViewModel.cs
@@ -1,7 +1,41 @@
 ﻿namespace YoutubePlayer.ViewModels
 {
-    public class StartPageViewModel
+    public class StartPageViewModel : AppViewModelBase
     {
+        public StartPageViewModel(IApiService appApiService) : base(appApiService)
+        {
+            this.Title = "YOUTUBE PLAYER";
+        }
+        public override async void OnNavigatedTo(object parameters)
+        {
+            SetDataLodingIndicators(true);
+
+            LoadingText = "Идёт загрузка";
+            //await Search();
+            try 
+            {
+                await Task.Delay(3000);
+                throw new Exception("Unable to reach Google Youtube API Service");
+                throw new InternetConnectionException();
+                this.DataLoaded = true;
+            }
+            catch (InternetConnectionException iex)
+            {
+                this.IsErrorState = true;
+                this.ErrorMessage = "Медленное или отсутствующее подключение к Интернету." + Environment.NewLine + "Пожалуйста, проверьте подключение к Интернету и повторите попытку.";
+                ErrorImage = "nointernet.png";
+            }
+            catch (Exception ex)
+            {
+                this.IsErrorState = true;
+                this.ErrorMessage = $"Что-то пошло не так. Если проблема сохраняется, обратитесь в службу поддержки по адресу: {Constants.EmailAddress} с сообщением об ошибке:" + Environment.NewLine + Environment.NewLine + ex.Message;
+                ErrorImage = "error.png";
+            }
+            finally 
+            {
+                SetDataLodingIndicators(false);
+            }
+        }
 
     }
 }
